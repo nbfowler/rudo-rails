0a5835edcd3e08da824d3bd3f762cad22713cd7b
nbfowler <nbfowler@gmail.com>
2011-04-05 20:00:01 -0700

    committing updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,20 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.unfinished_tasks
   end
 
   def create
     @task = Task.new(:title => params[:title])
-    @task.save
+    if @task.save
+      redirect_to '/'
+    else
+      @tasks = Task.unfinished_tasks
+      render "index"
+    end
+  end
+
+  def done
+    Task.find(params[:id]).update_attribute(:done_date, Time.now)
     redirect_to '/'
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   validates_presence_of :title
+
+  scope :unfinished_tasks, where(:done_date => nil)
 end
